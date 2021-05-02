7aa58335d5ae7031b152b14e2abddb2f249f5d4a
healing99 <nicealice99@gmail.com>
2021-05-02 17:03:51 +0900

    [step 3] API 결과 불러오기 + axios 설치

--- 3rd_seminar/github-finder/src/App.js
+++ 3rd_seminar/github-finder/src/App.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import SearchBar from './components/SearchBar';
 import ResultCard from './components/ResultCard';
+import { getUserData } from './lib/api/githubAPI';
 
 function App() {
+  useEffect(() => {
+    getUserData('healing99');
+  }, []);
   return (
     <>
       <h1>Hello</h1>
