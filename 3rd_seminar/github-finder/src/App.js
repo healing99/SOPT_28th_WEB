30f8add7cc69621244b2e750427f2d245551fe4d
healing99 <nicealice99@gmail.com>
2021-05-02 16:02:15 +0900

    [step 2] 전체 Component 구조 세팅

--- 3rd_seminar/github-finder/src/App.js
+++ 3rd_seminar/github-finder/src/App.js
@@ -1,7 +1,13 @@
+import React from 'react';
+import SearchBar from './components/SearchBar';
+import ResultCard from './components/ResultCard';
+
 function App() {
   return (
     <>
       <h1>Hello</h1>
+      <SearchBar />
+      <ResultCard />
     </>
   );
 }
