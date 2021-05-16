9f87b0e54545004a290cbc2416c8365cada9ce86
healing99 <nicealice99@gmail.com>
2021-05-16 16:43:44 +0900

    [step 6] Card 구현하기

--- 4th_seminar/diary-app/src/pages/Main.js
+++ 4th_seminar/diary-app/src/pages/Main.js
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import Card from "../components/main/Card";
+import styled from "styled-components";
+
+const MainWrap = styled.div`
+  display: grid;
+  grid-template-columns: repeat(5, auto);
+  row-gap: 25px;
+`;
 
 const Main = () => {
-  return <div>Main</div>;
+  //서버에서 API요청 결과값 받아오기 전에 먼저 dummy data로 구현
+  const [userData, setUserData] = useState({
+    id: 1,
+    date: 20210501,
+    title: "다이어리 제목입니다",
+    image: "",
+    weather: "맑음",
+    tags: ["태그1", "태그2"],
+  });
+  return (
+    <MainWrap>
+      <Card props={userData} />
+    </MainWrap>
+  );
 };
 
 export default Main;
